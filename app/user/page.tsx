e1531688d0fb7d8cc66223fa833b1fcadac481ef
khian calderon <ckhianvictory@gmail.com>
2025-08-03 12:27:07 +0800

    Added user page

---
+++ app/user/page.tsx
@@ -0,0 +1,10 @@
+export default function User() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+        <div className="text-center">
+          <h1 className="text-4xl font-semibold">User Page</h1>
+          <p className="text-sm">Correct, now try adding a username</p>
+        </div>
+    </div>
+  )
+}
