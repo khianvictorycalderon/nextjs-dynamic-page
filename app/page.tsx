39549995c8402e6f6fc27809378008ef278df917
khian calderon <ckhianvictory@gmail.com>
2025-08-03 12:25:40 +0800

    Updated home

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 export default function Home() {
   return (
-    <>
-    </>
+    <div className="w-screen h-screen flex items-center justify-center">
+        <div className="text-center">
+          <h1 className="text-4xl font-semibold">Home</h1>
+          <p className="text-sm">Try typing "user/{"<username>"}"</p>
+          <p className="text-sm">Choose any username you want</p>
+        </div>
+    </div>
   )
 }
